034f75c62216dc18a0751538b4eb922a55376b63
Piotr Mol <piotr94mol@gmail.com>
2023-03-13 05:46:06 +0100

    fix: add parsing

--- handler.js
+++ handler.js
@@ -7,12 +7,13 @@ const documentClient = new AWS.DynamoDB.DocumentClient();
 const userEmail = "test@email.com";
 
 module.exports.addNote = async (event) => {
-  const { noteText } = event.body;
+  const body = JSON.parse(event.body);
+  const { noteText } = body;
 
   const item = {
+    noteText,
     userEmail,
     noteDate: new Date().getTime(),
-    noteText,
   };
 
   const params = {
@@ -24,12 +25,17 @@ module.exports.addNote = async (event) => {
     await documentClient.put(params).promise();
     return {
       statusCode: 201,
-      body: JSON.stringify({ message: "Note added successfully" }),
+      body: JSON.stringify({
+        message: "Note added successfully",
+      }),
     };
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: "Cannot add note", error: err }),
+      body: JSON.stringify({
+        message: "Cannot add note",
+        error: err,
+      }),
     };
   }
 };
@@ -41,25 +47,29 @@ module.exports.deleteNote = async (event) => {
     TableName: "NoteTable",
     Key: {
       userEmail,
-      noteDate,
+      noteDate: +noteDate,
     },
   };
-
   try {
     await documentClient.delete(params).promise();
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Note deleted successfully" }),
+      body: JSON.stringify({
+        message: "Note deleted successfully",
+      }),
     };
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: "Cannot delete note", error: err }),
+      body: JSON.stringify({
+        message: "Cannot delete note",
+        error: err,
+      }),
     };
   }
 };
 
-module.exports.getAllNotes = async (event) => {
+module.exports.getAllNotes = async (_event) => {
   const params = {
     TableName: "NoteTable",
     KeyConditionExpression: "userEmail = :userEmail",
@@ -71,12 +81,17 @@ module.exports.getAllNotes = async (event) => {
     const notes = await documentClient.query(params).promise();
     return {
       statusCode: 200,
-      body: JSON.stringify({ notes }),
+      body: JSON.stringify({
+        notes,
+      }),
     };
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: "Cannot fetch notes", error: err }),
+      body: JSON.stringify({
+        message: "Cannot fetch note",
+        error: err,
+      }),
     };
   }
 };
